86be3c07fbaa37e12f7aeea308ccfeef5abc9655
thanhdang210203 <thanhdang210203@gmail.com>
2022-06-07 10:47:50 +0700

    fd

--- PLatformerGame/Assets/Scripts/pickUp.cs
+++ PLatformerGame/Assets/Scripts/pickUp.cs
@@ -23,7 +23,7 @@ public class pickUp : MonoBehaviour
     {
        if(collision.tag == "Heart")
         {
-            currentHealth += healthPickUp;
+            Debug.Log("Gain health");
         }
     }
 }
